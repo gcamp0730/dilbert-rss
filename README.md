0505f5e4f95ebc951c937ca9fa0eb65d77781cc2
Dennis Stengele <d.stengele@derintendant.de>
2015-01-16 00:52:22 +0100

    Fix merge fuckup

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 dilbert-rss
 ===========
 
+// Updated to work with new site (2015/01/15)//
+
 Scrapes dilbert.com and generates an RSS feed.
 
 Since dilbert.com nerfed their own RSS feed, I created a tool to replicate the old functionality. This is designed to be used on your own server, using `cron` to update it. I don't plan on hosting a replacement feed myself.
 
-You will need to install `BeautifulSoup` and `PyRSS2Gen` using `pip` or otherwise to run this script. Just run it from the command line with `python dilbert.py` to generate `dilbert.xml`.
+You will need to install `BeautifulSoup` and `PyRSS2Gen` using `pip` or otherwise to run this script.
+
+You can run the script by making it executable or by running `python3 dilbert.py`. It takes up to two arguments: The path where the .xml file will be saved, and the number of days to fetch for the feed. By default, the feed is saved in the working directory with ten items.
--- dilbert.py
+++ dilbert.py
@@ -17,9 +17,9 @@ outfile = "dilbert.xml"
 stripcount = 10
 
 if len(sys.argv) > 1:
-    outfile = sys.argv[1]
-    if len(sys.argv) > 2:
-        stripcount = sys.argv[2]
+	outfile = sys.argv[1]
+	if len(sys.argv) > 2:
+		stripcount = sys.argv[2]
 
 now = datetime.datetime.today()
 year = now.year
@@ -29,54 +29,38 @@ day = now.day
 strips = []
 
 for i in range(0, stripcount):
-    timedelta = datetime.timedelta(days=i)
-    currentDate = (now - timedelta)
-    currentDay = currentDate.day
-    currentMonth = currentDate.month
-    currentYear = currentDate.year
+	timedelta = datetime.timedelta(days=i)
+	currentDate = (now - timedelta)
+	currentDay = currentDate.day
+	currentMonth = currentDate.month
+	currentYear = currentDate.year
 
-    url = "http://dilbert.com/strip/" + str(year) + "-" + str(month) + "-" + str(currentDay)
-    request = http.request("GET", url)
+	url = "http://dilbert.com/strip/" + str(year) + "-" + str(month) + "-" + str(currentDay)
+	request = http.request("GET", url)
 
-    if request.status == 200:
-        page = request.data
-        soup = BeautifulSoup(page)
+	if request.status == 200:
+		page = request.data
+		soup = BeautifulSoup(page)
 
-        imageURL = soup.find_all('img', {'class':'img-comic'}, limit=1)[0]['src']
+		imageURL = soup.find_all('img', {'class':'img-comic'}, limit=1)[0]['src']
 
-        item = PyRSS2Gen.RSSItem(
-            title = 'Comic for ' + str(currentYear) + '/' + str(currentMonth) + '/' + str(currentDay),
-            description = "<a href='" + url + "'><img src='" + imageURL + "' /></a>",
-            pubDate = currentDate,
-            link = url,
-            guid = PyRSS2Gen.Guid(url)
-        )
-        strips.append(item)
-        return results
-    else:
-        progress= (i/stripcount) + (1 / stripcount)
+		item = PyRSS2Gen.RSSItem(
+			title = 'Comic for ' + str(currentYear) + '/' + str(currentMonth) + '/' + str(currentDay),
+			description = "<a href='" + url + "'><img src='" + imageURL + "' /></a>",
+			pubDate = currentDate,
+			link = url,
+			guid = PyRSS2Gen.Guid(url)
+		)
+		strips.append(item)
+		progress= (i/stripcount) + (1 / stripcount)
+		print("{:.0%}".format(progress))
 
-url = 'http://dilbert.com'
-request = http.request("GET", url)
-if request.status == 200:
-    page = request.data
-    soup = BeautifulSoup(page)
-        print("{:.0%}".format(progress))
-    strips = []
+	# Construct RSS
+	rss = PyRSS2Gen.RSS2(
+		title = "Dilbert Daily Strip",
+		link = "http://dilbert.com",
+		description = "An unofficial RSS feed for dilbert.com.",
+		lastBuildDate = now,
+		items = strips)
 
-    for i in range(0,50):
-        details = getDetails(nextUrl,url)
-        strips.append(details['item'])
-        nextUrl = url + details['prev_href']
-
-    # Construct RSS
-    rss = PyRSS2Gen.RSS2(
-        title = "Dilbert Daily Strip",
-        link = "http://dilbert.com",
-        description = "An unofficial RSS feed for dilbert.com.",
-        lastBuildDate = now,
-        items = strips)
-
-    rss.write_xml(open(outfile, "w"))
-else:
-    sys.exit(1)
+	rss.write_xml(open(outfile, "w"))
